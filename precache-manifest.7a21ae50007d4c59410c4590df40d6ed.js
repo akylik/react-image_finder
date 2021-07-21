365c5b935c065400eaf5fb6583e38bcbb2342ebd
Kulyk Oleksandr <70694467+akylik@users.noreply.github.com>
2021-07-21 12:17:50 +0300

    Updates

---
+++ precache-manifest.7a21ae50007d4c59410c4590df40d6ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a235913013eed8eb4a6dc2b59d93d1c",
+    "url": "/react-image_finder/index.html"
+  },
+  {
+    "revision": "a90f130c09c8d31acf0b",
+    "url": "/react-image_finder/static/css/2.1a215020.chunk.css"
+  },
+  {
+    "revision": "d16b920af5588728e0fc",
+    "url": "/react-image_finder/static/css/main.65b109a6.chunk.css"
+  },
+  {
+    "revision": "a90f130c09c8d31acf0b",
+    "url": "/react-image_finder/static/js/2.c1cdb0de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-image_finder/static/js/2.c1cdb0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16b920af5588728e0fc",
+    "url": "/react-image_finder/static/js/main.7bb61b1c.chunk.js"
+  },
+  {
+    "revision": "fd896899d378aca47755",
+    "url": "/react-image_finder/static/js/runtime-main.77e5c715.js"
+  }
+]);
